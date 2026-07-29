940f3cd5e2da401b8e6898f5387adccf77f3840f
Aravind Myathar <aravindmyathar@gmail.com>
2026-07-29 16:07:22 +0530

    fix: added the missed await for action methods

--- src/test/digitLogin.spec.ts
+++ src/test/digitLogin.spec.ts
@@ -3,13 +3,13 @@ import {test,expect} from "../fixtures/TestFixtures";
 test.describe("DigitLogin tests",{tag:"@digit"}, () => {
     test("Digit login with valid credentials", async ({ page, login }) => {
         
-        login.navigateToApplication();
-        login.enterEmail(`${process.env.DIGIT_EMAIL}`);
-        login.clickContinue();
-        login.enterPassword(`${process.env.DIGIT_PASSWORD}`);
-        login.clickContinue();
+        await login.navigateToApplication();
+        await login.enterEmail(`${process.env.DIGIT_EMAIL}`);
+        await login.clickContinue();
+        await login.enterPassword(`${process.env.DIGIT_PASSWORD}`);
+        await login.clickContinue();
         await page.waitForLoadState('networkidle');
-        expect(page).toHaveTitle(/Scorecard/);
-        expect(page).toHaveURL(/scorecard/);
+        await expect(page).toHaveTitle(/Scorecard/);
+        await expect(page).toHaveURL(/scorecard/);
     })
 })
