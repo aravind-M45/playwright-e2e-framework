6695af852257e1928624493885fa0cfb85de25c1
Aravind Myathar <aravindmyathar@gmail.com>
2026-08-03 13:20:00 +0530

    refactor the price list

--- src/test/digitPricelist.spec.ts
+++ src/test/digitPricelist.spec.ts
@@ -1,29 +1,34 @@
-import {test,expect} from "@playwright/test";
-import {DigitLoginPage} from "../pages/digitLogin.page"
+import { test, expect } from "@playwright/test";
+import { DigitLoginPage } from "../pages/digitLogin.page"
 
-test("Price List",async ({page})=>{
-    let login=new DigitLoginPage(page);
+test("Price List", async ({ page }) => {
+    let login = new DigitLoginPage(page);
     await login.navigateToApplication();
     await login.enterEmail(`${process.env.DIGIT_EMAIL}`)
     await login.clickContinue()
     await login.enterPassword(`${process.env.DIGIT_PASSWORD}`)
     await login.clickContinue()
     await expect(page).toHaveTitle(/Scorecard/);
-    await expect(page).toHaveURL(/scorecard/);
-
-    await page.getByText("Price lists",{exact:true}).click();
-    await page.getByRole("button",{name:/New price list/i}).click();
+    await expect(page).toHaveURL(/scorecard/)
+    await page.getByText("Price lists", { exact: true }).click();
+    await page.getByRole("button", { name: /New price list/i }).click();
     await page.getByRole('textbox', { name: /Name/i }).fill('Test_Pricelist01');
     await page.locator('span').filter({ hasText: 'Search to add...' }).first().click();
     await page.getByPlaceholder("Search all items").fill("Asus TUF - A15 Laptop");
-    await page.getByRole('checkbox', { name: 'Toggle select row' }).check();
+    const laptopRow = page.getByRole('row', { name: /Asus TUF - A15 Laptop/ });
+    await laptopRow.getByRole('checkbox', { name: 'Toggle select row' }).check();
+
     await page.getByRole('button', { name: /Add items/i }).click();
     await page.getByText('Customers', { exact: true }).click();
-    await page.getByRole('button',{name:/Add customer/i}).click();
+    await page.getByRole('button', { name: /Add customer/i }).click();
+
     await page.getByPlaceholder("Search...").first().fill("Adidas");
-    await page.getByRole('checkbox', { name: 'Press Space to toggle row selection (unchecked)' }).check();
-    await page.getByRole('button',{name:/Add customers/i}).last().click();
+    await expect(page.getByRole('row', { name: /Adidas/ })).toBeVisible();
+    const adidasRow = page.getByRole('row', { name: /Adidas/ });
+    await adidasRow.getByRole('checkbox', { name: 'Press Space to toggle row' }).check();
+
+    await page.getByRole('button', { name: /Add customers/i }).last().click();
     await page.getByRole('button', { name: /Save/i }).click();
-    await expect(page.locator('p').filter({hasText:'Test_Pricelist01'})).toBeVisible();
+    await expect(page.locator('p').filter({ hasText: 'Test_Pricelist01' })).toBeVisible();
 
 })
