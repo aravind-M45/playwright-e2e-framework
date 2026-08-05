b3f4ea58cbc1d31f60c5c30ced76f2900a7f773a
Aravind Myathar <aravindmyathar@gmail.com>
2026-08-05 15:18:11 +0530

    post api request for create booking

---
+++ tests/API_Post_createBooking.spec.ts
@@ -0,0 +1,32 @@
+
+import {test,expect} from "@playwright/test"
+import PostAPIBookingData from "../tests/TestData/API_testdata/POST_BookingDetails.json"
+
+
+test.use({
+    baseURL:"https://restful-booker.herokuapp.com",
+})
+test("POST API request using static file to Create Booking",async ({request})=>{
+    const postAPIResponse=await request.post("/booking",{data:PostAPIBookingData})
+    const postJSONResponse=await postAPIResponse.json()
+    console.log(postJSONResponse)
+
+    //Valaidate the response
+    expect(postAPIResponse.status()).toBe(200)
+    expect(postAPIResponse.statusText()).toBe("OK")
+
+    //validate the headers
+    expect(postAPIResponse.headers()['content-type']).toBe("application/json; charset=utf-8")
+
+    //validate response body property/keys
+    expect(postJSONResponse.booking).toHaveProperty("firstname")
+    expect(postJSONResponse.booking).toHaveProperty("lastname")
+    expect(postJSONResponse.booking.bookingdates).toHaveProperty("checkin")
+    expect(postJSONResponse.booking.bookingdates).toHaveProperty("checkout")
+
+    expect(postJSONResponse.booking.firstname).toBe(PostAPIBookingData.firstname)
+    expect(postJSONResponse.booking.lastname).toBe(PostAPIBookingData.lastname)
+    expect(postJSONResponse.booking.bookingdates.checkin).toBe(PostAPIBookingData.bookingdates.checkin)
+    expect(postJSONResponse.booking.bookingdates.checkout).toBe(PostAPIBookingData.bookingdates.checkout)
+
+})
