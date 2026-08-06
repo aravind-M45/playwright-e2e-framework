c94ab052b229f67afed96e552aa724001a202450
Aravind Myathar <aravindmyathar@gmail.com>
2026-08-06 15:02:30 +0530

    tests: generating random data at the runtime using faker

--- tests/API_Post_DynamicBooking.spec.ts
+++ tests/API_Post_DynamicBooking.spec.ts
@@ -3,7 +3,7 @@ import {formatAPIRequest} from "../src/utils/APIHelpers";
 
 import path from "path";
 import fs from "fs";
-
+import {faker} from "@faker-js/faker";
 
 test.use({
     baseURL:"https://restful-booker.herokuapp.com",
@@ -14,6 +14,26 @@ test("POST API request using dynamic file to create booking",async({request})=>{
     const postAPIBooking = fs.readFileSync(jsonPath,"utf-8");
 
     const values=["Dynamic Test Data", "Playwright API testing", 4500];
+    const postAPIBookingData=await formatAPIRequest(postAPIBooking,values)
+
+    const postAPIResponse=await request.post("/booking",{data:JSON.parse(postAPIBookingData)})
+    const postJSONResponse=await postAPIResponse.json();
+    console.log("POST API Response: ",postJSONResponse);
+    expect(postAPIResponse.status()).toBe(200);
+    expect(postAPIResponse.statusText()).toBe("OK");
+    expect(postAPIResponse.headers()['content-type']).toContain("application/json; charset=utf-8");
+    expect(postJSONResponse.booking).toHaveProperty("totalprice");
+    expect(postJSONResponse.booking).toHaveProperty("firstname");
+
+})
+test("POST API request using dynamic file to create booking using faker",async({request})=>{
+    const jsonPath = path.join(__dirname,"../tests/TestData/API_testdata/POST_DynamicDetails.json");
+    const postAPIBooking = fs.readFileSync(jsonPath,"utf-8");
+
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const totalprice = faker.number.int({min:1000,max:10000});
+    const values=[firstName, lastName, totalprice];
     const postAPIBookingData=await formatAPIRequest(postAPIBooking,values)
 
     const postAPIResponse=await request.post("/booking",{data:JSON.parse(postAPIBookingData)})
