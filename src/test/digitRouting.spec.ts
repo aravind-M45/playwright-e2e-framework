5906ab35a258a04de7e5dc118cabd302eeee8629
Aravind Myathar <aravindmyathar@gmail.com>
2026-08-18 16:45:33 +0530

    test: Update and Delete Operations for Routing Step

--- src/test/digitRouting.spec.ts
+++ src/test/digitRouting.spec.ts
@@ -1,10 +1,21 @@
 import { test, expect } from "../fixtures/TestFixtures";
-import Routing from "../pages/digitRouting.page"
+import Routing from "../pages/digitRouting.page";
 
-test("Production Routing", async ({ page, login }) => {
-    test.setTimeout(50000);
-    await login.userLogin(`${process.env.DIGIT_EMAIL}`, `${process.env.DIGIT_PASSWORD}`);
-    await login.verifyLogin();
-    const routing=new Routing(page);
-    await routing.createRoutingStep('Test Routing Step')
-})
+test.describe("Digit Routing", () => {
+
+    test.beforeEach("User Login", async ({ login }) => {
+        await login.userLogin(`${process.env.DIGIT_EMAIL}`, `${process.env.DIGIT_PASSWORD}`);
+        await login.verifyLogin();
+    });
+    
+    test("Verify Create, Update, and Delete Routing Step",{ tag: "@digit" },async ({ page }) => {
+            test.setTimeout(50000);
+            const routing = new Routing(page);
+            await routing.createRoutingStep('Test Routing Step');
+            await routing.updateRoutingStep(); 
+            await routing.deleteRoutingStep(); 
+        }
+    );
+
+
+});
