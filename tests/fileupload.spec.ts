652714a12aa4ba626cfc17a9d5b5e45b4ca5a6f0
Aravind Myathar <aravindmyathar@gmail.com>
2026-08-21 16:38:39 +0530

    refactor: upload and download spec file

--- tests/fileupload.spec.ts
+++ tests/fileupload.spec.ts
@@ -1,25 +1,32 @@
-import { test, expect } from "@playwright/test"
+import { test, expect } from "@playwright/test";
 
-test("Verify File Uploads", async ({ page }) => {
+test.describe("File Upload and Download", () => {
 
-    await page.goto("https://testautomationpractice.blogspot.com/");
-    const fileupload = page.locator('[id="singleFileInput"]');
-    await expect(fileupload).toBeVisible();
-    await fileupload.setInputFiles('./tests/uploads/fileupload.json');
-    await expect(fileupload).toHaveValue(/fileupload\.json/);
-})
+    test.beforeEach("User Login",async ({ page }) => {
+        await page.goto(
+            "https://bakkappan.github.io/Testers-Talk-Practice-Site/"
+        );
+        await page.getByPlaceholder("Username").fill("TestersTalk");
+        await page.getByPlaceholder("Password").fill("TestersTalk");
+        await page.getByRole("button", { name: "Login" }).click();
+        await expect(page.locator("#welcomeMsg")).toBeVisible();
+    });
 
-test("Verify File Download", async ({ page }) => {
-    test.setTimeout(30000)
-    await page.goto("https://bakkappan.github.io/Testers-Talk-Practice-Site/");
-    await page.getByPlaceholder("Username").fill("TestersTalk");
-    await page.getByPlaceholder("Password").fill("TestersTalk");
-    await page.getByRole('button', { name: "Login" }).click();
-    const [download]=await Promise.all([
-        page.waitForEvent('download'),
-        page.getByRole('link',{name:'Download Excel'}).click()
-    ])
-    console.log('Downloaded File Name: ',download.suggestedFilename());
-    await download.saveAs('./download/FullCourse.xlsr')
+    test("Verify File Upload", async ({ page }) => {
+        const fileUpload = page.locator("#fileInput");
+        await fileUpload.setInputFiles(
+            "./tests/uploads/fileupload.json"
+        );
+        await expect(page.locator("#fileName"))
+            .toHaveText("Selected: fileupload.json");
+    });
 
-})
+    test("Verify File Download", async ({ page }) => {
+        const [download] = await Promise.all([
+            page.waitForEvent("download"),
+            page.getByRole("link", { name: "Download Excel" }).click()
+        ]);
+        console.log("Downloaded File Name:",download.suggestedFilename());
+        await download.saveAs("./download/FullCourse.xlsx");
+    });
+});
